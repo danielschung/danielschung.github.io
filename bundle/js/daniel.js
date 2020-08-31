afdb3d35d151e5a1a3dfb33b5d4c0ee22eecbe26
Daniel Chung <danielschung@icloud.com>
2020-08-31 18:09:45 -0500

    mobile fixes

--- bundle/js/daniel.js
+++ bundle/js/daniel.js
@@ -12,12 +12,12 @@ $('div.mobile-nav').click( function() {
   $('div.links a').fadeIn(1000);
   $('nav').animate({width:'202px'},200,'swing');
 
-  $('div.mobile-nav div.line:nth-child(2)').animate({width:'100%'},200,'swing');
+  $('div.mobile-nav div.line:nth-child(1)').animate({width:'100%'},200,'swing');
 
   // click wrapper to make nav leave
   $('.wrapper').one('click', function() {
     $('nav').animate({width:'0px'},200,'swing');
-    $('div.mobile-nav div.line:nth-child(2)').animate({width:'67%'},200,'swing');
+    $('div.mobile-nav div.line:nth-child(1)').animate({width:'67%'},200,'swing');
   });
 });
 
