2f4204bfb7cc42daab94e8db4e82889c288686d3
Daniel.Chung <dchung@NB72649.local>
2021-07-08 14:13:07 -0500

    initial commit 2.0

--- bundle/js/daniel.js
+++ bundle/js/daniel.js
@@ -1,76 +1,38 @@
-// --- Description: basic functionality
-// Table of Contents
-//  1. Mobile Nav
-//  2. Anime Functions
-//  3. Anime Deploy on Scroll
-
 $(document).ready( function() {
 
-//  1. Mobile Nav -----
-    
-$('div.mobile-nav').click( function() {
-  $('div.logo').fadeIn(1000);
-  $('div.links a').fadeIn(1000);
-  $('div.social').fadeIn(1000);
-  $('nav').animate({width:'252px'},200,'swing');
-  $('div.mobile-nav div.line:nth-child(1)').animate({width:'100%'},200,'swing');
-  $('div.mobile-nav div.close').show();
-
-  // click wrapper or hamburger to make nav leave
-  $('.wrapper, div.close').one('click', function(event) {
-    event.stopPropagation();
-    $('nav').animate({width:'0px'},200,'swing');
-    $('div.mobile-nav div.line:nth-child(1)').animate({width:'67%'},200,'swing');
-    $('div.logo').hide();
-    $('div.links a').hide();
-    $('div.social').hide();
-    $('div.mobile-nav div.close').hide();
-  });
-
-});
-
-//  2. Anime Functions -----
-
+//  Animations -----
 function introFade(object) {
+  $(object).fadeIn(1000);
   $(object).css('opacity','0');
   $(object).fadeTo(977, 1, 'swing');
 }
 
-function hide(object) {
-  $(object).css('opacity','0');
+function slideIn(object) {
+  $(object).animate({right:'0'}, 300);
 }
 
-function fade(object) {
-  $(object).fadeTo(770, 1, 'swing');
-}
+introFade('.fade'); // run fade animation
 
-function scrollFade(object) {
-  $(object).each( function(i){
-    // let bottom_of_element = $(this).offset().top + $(this).outerHeight();
-    let top_of_element = $(this).offset().top;
-    let bottom_of_window = $(window).scrollTop() + $(window).height();
-    if( bottom_of_window > top_of_element ){
-      fade($(this));
-    }
+//  Flux transitional effects -----
+//  --- to BLUE (R to L)
+$('a.flux.blue').click( function(e) {
+  e.preventDefault();
+  var link = $(this);
+  $('.fade').fadeOut( function() {
+    $('div.blue').animate({width:'100vw'}, 500, function() {
+      document.location = link.attr('href');
+    });
   });
-}
-
-//  3. Anime Deploy -----
-
-introFade('.wrapper *');
-
-// hide('section.wrapper *');
-// hide('footer.wrapper *');
-// scrollFade('section.wrapper *');
-// scrollFade('footer.wrapper *');
-
-
-// $(window).scroll( function(){ // deploy on scroll
-//   scrollFade('section.wrapper *');
-//   scrollFade('footer.wrapper *');
-// });
-
+})
+//  --- to WHITE (L to R)
+$('a.flux.white').click( function(e) {
+  e.preventDefault();
+  var link = $(this);
+  $('.fade').fadeOut( function() {
+    $('div.white').animate({width:'50vw'}, 500, function() {
+      document.location = link.attr('href');
+    });
+  });
+})
 
 });
-
-
